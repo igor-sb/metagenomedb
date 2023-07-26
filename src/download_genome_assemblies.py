326399d8fd19271022e3891f7de4715bb8737ec5
igor-sb <>
2023-07-26 08:32:13 -0700

    mypy wont complain about missing types from libraries

--- src/download_genome_assemblies.py
+++ src/download_genome_assemblies.py
@@ -1,8 +1,7 @@
 """Download genome assembly summaries from NCBI FTP."""
 
 import logging
-
-import fire  # type: ignore
+import fire
 
 from src.genome_assemblies.download import download_assembly_summaries
 
