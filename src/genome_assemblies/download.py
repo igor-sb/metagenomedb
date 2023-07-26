80b195417507436cd56a84c2bea30735e54ef47c
igor-sb <>
2023-07-25 17:35:48 -0700

    add unit test for downloads

--- src/genome_assemblies/download.py
+++ src/genome_assemblies/download.py
@@ -3,14 +3,18 @@
 import os
 import fire
 import logging
-import urllib.request
+from email.message import Message
+from urllib.request import urlretrieve
 from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
-def download_kingdom_assembly_summary(kingdom, output_filename):
+def download_kingdom_assembly_summary(
+    kingdom: str,
+    output_filename: str
+) -> tuple[str, Message]:
     base_url = 'ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/'
     full_url = f'{base_url}/{kingdom}/assembly_summary.txt'
 
@@ -18,22 +22,25 @@ def download_kingdom_assembly_summary(kingdom, output_filename):
     if not os.path.isdir(output_path):
         os.makedirs(output_path)
 
-    urllib.request.urlretrieve(full_url, output_filename)  # noqa: S310
+    return urlretrieve(full_url, output_filename)  # noqa: S310
 
 
 def download_assembly_summaries(
-    output_path,
-    kingdoms='archaea,bacteria',
-    timestamp=True,
-):
+    output_path: str,
+    kingdoms: str='archaea,bacteria',
+    timestamp: bool=True,
+) -> list[str]:
+    filenames = []
     suffix = '_' + datetime.now().strftime('%Y-%m-%d') if timestamp else ''
     for kingdom in kingdoms.split(','):
-        LOG.info(' downloading genome assembly summary for kingdom: {kingdom}')
+        LOG.info(f' downloading genome assembly summary for kingdom: {kingdom}')
         output_filename = os.path.join(
             output_path,
             f'{kingdom}_assembly_summary{suffix}.txt',
         )
-        download_kingdom_assembly_summary(kingdom, output_filename)
+        file, _ = download_kingdom_assembly_summary(kingdom, output_filename)
+        filenames.append(file)
+    return filenames
 
 
 if __name__ == '__main__':
